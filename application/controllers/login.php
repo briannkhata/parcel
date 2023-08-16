e3cc50345135aae483feb195f181b82c1066a30c
Brian Nkhata <bnkhata@mra.mw>
2023-08-16 16:18:36 +0200

    house keeping

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -5,19 +5,18 @@ class Login extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct(); 
-    } 
+	} 
 
 	
 	
 	function signin()
 	{   
-	
 		if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password']))
 				{
-				    $username	=	$this->input->post('username');
-				    $password	=	MD5($this->input->post('password'));			  
-				    $admin      =   $this->db->query("SELECT * FROM users WHERE username='$username' AND password='$password'");
-				    $row = $admin->row();
+					$username	=	$this->input->post('username');
+					$password	=	MD5($this->input->post('password'));			  
+					$admin      =   $this->db->query("SELECT * FROM users WHERE username='$username' AND password='$password'");
+					$row = $admin->row();
 						if (isset($row))
 						{
 								
@@ -25,23 +24,20 @@ class Login extends CI_Controller {
 									$password	=	$row->password;
 									$fname		=	$row->fname;
 									$lname		=	$row->lname;
-									$user_id	=	$row->user_id;
+									$userId	=	$row->userId;
 													
 									$this->session->set_userdata('user_login', '1');
-									$this->session->set_userdata('user_id',$user_id);
+									$this->session->set_userdata('userId',$userId);
 									$this->session->set_userdata('account_type','admin');
-									redirect(base_url() .'admin/dashboard', 'refresh');
+									redirect(base_url() .'User/dashboard', 'refresh');
 						}
-										   
-									   
-								$page_data['system']  	  = 'payrollSoft';
+
+								$page_data['system']  	  = 'Parcel';
 								$page_data['page_title']  = 'Login';
-						    	//$page_data['err'] 	  = 'Invalid Username or Password';	
-						    	$this->session->set_flashdata('message','Invalid Username or Password');
+								$this->session->set_flashdata('message','Invalid Username or Password');
 								return $this->load->view('index',$page_data);
 										   
-				
-			    }
+				}
 				
 				
 	}
--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
-
 		function __construct()
 		{
 			parent::__construct();
@@ -9,14 +8,9 @@ class Welcome extends CI_Controller {
 		
 		function index()
 		{		
-				$page_data['system']  	  = 'payrollSoft';
-				$page_data['page_title']  = 'Login';
-				$page_data['status'] 	  = ''; // shows blank when new updated db is not uploaded
-				//$this->session->set_userdata('my_database','payroll');
-				$this->load->view('index',$page_data);	
+			$page_data['system']  	  = 'payrollSoft';
+			$page_data['page_title']  = 'Login';
+			$this->load->view('index',$page_data);	
 		}
 		
-		
-		
-	
 }
